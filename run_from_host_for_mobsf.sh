5df5cbc9b14ceca9455ffb7490761db228dff18a
Drew Green <agreenbhm@gmail.com>
2024-08-01 16:54:02 -0400

    Updated local machine script.

--- run_from_host_for_mobsf.sh
+++ run_from_host_for_mobsf.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: ./$@ <container-name> <emulator-name>"
+    echo "Usage: $0 <container-name> <emulator-name>"
+    exit
 fi
 
 CONTAINER_NAME="$1"
 EMULATOR_NAME="$2"
 
+echo "Patching MobSF to support Android API 34"
 docker exec -it $CONTAINER_NAME bash -c "sed -i 's/^ANDROID_API_SUPPORTED.*$/ANDROID_API_SUPPORTED = 34/g' /home/mobsf/Mobile-Security-Framework-MobSF/mobsf/DynamicAnalyzer/views/android/environment.py"
+echo "Restarting container"
 docker restart $CONTAINER_NAME
-adb -s $EMULATOR_NAME shell "su -c 'mount -o remount,rw /system'"
+echo "Mounting emulator /system as r/w using OverlayFS"
+adb -s $EMULATOR_NAME shell "su -c 'mount -o remount,rw /system; setenforce 0'"
+echo "Disabling SELinux to fix issue with settings service"
+adb -s $EMULATOR_NAME shell "su -c 'setenforce 0'"
